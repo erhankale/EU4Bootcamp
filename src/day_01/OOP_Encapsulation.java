a0ef05768a8abf371bfc15dc769063201330b589
erhankale <erhankal3@gmail.com>
2021-03-29 22:53:35 +0200

    OOP_Encapsulation added

--- src/day_01/OOP_Encapsulation.java
+++ src/day_01/OOP_Encapsulation.java
@@ -2,5 +2,33 @@ package day_01;
 
 public class OOP_Encapsulation {
 
-    //
+    //name, id
+
+    //if one of the instances is FINAL you don't need to crate setter method
+    private String name;
+    private int id;
+
+    public OOP_Encapsulation() {
+    }
+
+    public OOP_Encapsulation(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public String getName()  {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }

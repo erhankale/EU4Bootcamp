0d2fc024f54841be0375faaf4fe1ee5a3b9f1b2b
erhankale <erhankal3@gmail.com>
2021-03-29 19:39:48 +0200

    updated

--- src/day_01/Loops.java
+++ src/day_01/Loops.java
@@ -1,4 +1,36 @@
 package day_01;
 
 public class Loops {
+
+
+    public static void main(String[] args) {
+
+        for (int i = 1 ; i<6 ;i++ ) { //-->> if there is no condition it will run infinitive
+            System.out.println("Hello World");
+        }
+
+
+        System.out.println("=======================");
+
+        String str = "hello";
+
+        String reversed = "";
+
+//        reversed += str.charAt( str.length()-1);
+
+//        reversed += str.charAt(4);
+//        reversed += str.charAt(3);
+//        reversed += str.charAt(2);
+//        reversed += str.charAt(1);
+//        reversed += str.charAt(0);
+
+        for (int i = str.length()-1 ; i>=0 ; i--){
+            reversed += str.charAt(i);
+        }
+
+        System.out.println("reversed = " + reversed);
+
+    }
+
+
 }

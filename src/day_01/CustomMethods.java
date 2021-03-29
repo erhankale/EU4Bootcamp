fd6f6558d9a52ed25ece85bbe20ed3cdbf610ae3
erhankale <erhankal3@gmail.com>
2021-03-29 20:14:57 +0200

    CustomMethods created

---
+++ src/day_01/CustomMethods.java
@@ -0,0 +1,32 @@
+package day_01;
+
+public class CustomMethods {
+
+    public static void main(String[] args) {
+
+        String str1 = "Cybertek";
+
+        String reversed1 = "";
+
+        for (int i = str1.length()-1 ; i >=0 ; i--) {
+        reversed1 += str1.charAt(i);
+        }
+
+        String str2 = "School";
+
+        System.out.println(reverseOfWord(str2));
+
+        String str3 = "Java";
+
+        System.out.println(reverseOfWord(str3));
+
+    }
+
+    public static String reverseOfWord(String word){
+        String reversed = "";
+        for (int i = word.length()-1 ; i >=0 ; i--) {
+            reversed += word.charAt(i);
+        }
+        return reversed;
+    }
+}

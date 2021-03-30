1557e9d8f4d0da9a07c700c94fcddfe7fa1ea1af
erhankale <erhankal3@gmail.com>
2021-03-30 23:20:01 +0200

    MapPractice added

--- src/day_02/MapPractice.java
+++ src/day_02/MapPractice.java
@@ -1,4 +1,24 @@
 package day_02;
 
+import java.util.LinkedHashMap;
+
 public class MapPractice {
+
+    public static void main(String[] args) {
+        String[] namesGroup1 = {"Oliver", "Jack", "Noah", "Breanna", "Shay","James", "Conor", "Jake", "John"};
+        Integer[] salariesGroup1 = {11000, 105000, 95000, 85000, 100000, 90000, 78000, 118000, 98000};
+
+        LinkedHashMap<String, Integer> group1 = new LinkedHashMap<>();
+
+        for(int i= 0; i < namesGroup1.length; i++){
+            group1.put( namesGroup1[i], salariesGroup1[i]  );
+        }
+
+
+        System.out.println(group1);
+
+
+
+    }
+
 }

cc95ecd3d81fb12074e25ab2735cc6d089f0c026
erhankale <erhankal3@gmail.com>
2021-03-30 19:02:58 +0200

    OOP_Inheritance added

---
+++ src/day_01/OOP_Inheritance.java
@@ -0,0 +1,60 @@
+package day_01;
+
+class Car{
+    public String brand, model, color;
+    public double price;
+
+    public Car(String brand, String model, String color, double price) {
+        this.brand = brand;
+        this.model = model;
+        this.color = color;
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "brand='" + brand + '\'' +
+                ", model='" + model + '\'' +
+                ", color='" + color + '\'' +
+                ", price= $" + price +
+                '}';
+    }
+
+}
+
+class Toyota extends Car{
+
+    public Toyota(String model, String color, double price){
+        super("Toyota", model, color, price );
+    }
+
+}
+
+class Mercedes extends Car{
+
+    public Mercedes( String model, String color, double price) {
+        super("Mercedes", model, color, price);
+    }
+
+}
+
+class BMW extends Car{
+
+    public BMW( String model, String color, double price) {
+        super("BMW", model, color, price);
+    }
+}
+
+public class OOP_Inheritance{
+
+    public static void main(String[] args) {
+        Toyota toyota = new Toyota("Camry", "White", 15000);
+        Mercedes mercedes = new Mercedes("AMG", "Black", 60000);
+        BMW bmw = new BMW("X7", "Red", 75000);
+
+        System.out.println(toyota);
+        System.out.println(mercedes);
+        System.out.println(bmw);
+    }
+}
